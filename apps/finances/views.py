a537b2424565aee7259a504c3ba522982265e36f
Oseni Ayomide <oseniayomide57@gmail.com>
2023-09-07 12:10:48 +0100

    configured subscription system with stripe

--- apps/finances/views.py
+++ apps/finances/views.py
@@ -14,19 +14,28 @@ class PricingView(TemplateView):
         return context 
 
 
-def PricingPaymentMethod(View):
+def PricingPaymentMethod(LoginRequiredMixin, View):
     
     def get(self, request, price_id, *args, **kwargs):
+        price = Price.objects.get(id=price_id)
         context = {
-            "price": Price.objects.prefetch_related("product").get(id=price_id)
+            "price": price
         }
         intent = PaymentIntentForm(data={"price": price})
         intent.save(request.user)
         context["client_secret"] = intent.client_secret
+        context["payment_intent_id"] = intent.id
         context["STRIPE_PUBLISHABLE_KEY"] = settings.STRIPE_PUBLISHABLE_KEY
         return render(request, "finances/card.html", context)
     
-    def post(self, request, id, *args, **kwargs):
-        pass
+    def post(self, request, *args, **kwargs):
+        payment_intent_id = request.POST.get("payment_intent_id")
+        payment_method_id = request.POST.get("payment_method_id")
         
+        stripe.PaymentIntent.modify(
+            payment_intent_id,
+            payment_method=payment_method_id
+        )
         
+        stripe.PaymentIntent.confirm(payment_method_id)
+        return render(request, "finances/payment_successful.html")
--- static/js/card.js
+++ static/js/card.js
@@ -1,5 +1,74 @@
 function card(stripe_publishable_key, customer_email {
     document.addEventListener("DOMContentLoader", function(event) {
         var stripe = Stripe(stripe_publishable_key);
+        var elements = stripe.elements();
+        
+        var style = {
+            base: {
+                color: "#32325d",
+                fontFamily: "Helvetica Neue", "Helvetica, sans-serif",
+                fontSmoothing: "antialiased",
+                fontSize: "16px",
+                "::placeholder": {
+                    color: "#aab7c4"
+                }
+            },
+            invalid: {
+                color: "#fa755a",
+                iconColor: "#fa755a",
+            }
+        };
+        
+        // Create an instance of the card element 
+        var card = elements.create("card", {style: style});
+        
+        card.mount("#card-element");
+        
+        // Handle real-time error validation 
+        card.addEventListener("change", function(event) {
+            var displayError = document.getElementById("card-errors");
+            
+            if (event.error) {
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = "";
+            }
+        });
+        
+        // Handle form submission 
+        var form = document.getElementById("payment-form");
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            
+            strip.createToken(card).then(function(result) {
+                if (result.error) {
+                    var errorElement = document.getElementById("card-errors");
+                    errorElement.textContent = result.error.message;
+                } else {
+                    // Create payment method
+                    stripe.createPaymemtMethod({
+                        type: "card",
+                        card: card,
+                        billing_details: {
+                            email: customer_email,
+                        }
+                    }).then(function(payment_method_result) {
+                        if (payment_method_result.error) {
+                            var errorElement = document.getElementById("card-errors");
+                            errorElement.textContent = payment_method_result.error.message;
+                        } else {
+                            var form = document.getElementById('payment-form');
+                            var hidden_input = document.createElement("input");
+                            
+                            hidden_input.setAttribute("type", "hidden");
+                            hidden_input.setAttribute("name", "payment_method_name");
+                            hidden_input.setAttribute("value", payment_method_result.paymentMethod.id);
+                            form.appenChild(hidden_input);
+                            form.submit();
+                        }
+                    })
+                }
+            })
+        })
     })
 }) 
