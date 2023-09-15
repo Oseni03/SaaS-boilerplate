9c42a9066acaa9af4afe889a7580a1d125cd9770
Oseni Ayomide <oseniayomide57@gmail.com>
2023-09-15 06:12:35 +0100

    added django-debug-toolbar

--- config/urls/main.py
+++ config/urls/main.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+import debug_toolbar
 
 urlpatterns = [
     path('finances/', include('apps.finances.urls.main', namespace="finances")),
@@ -7,3 +10,8 @@ urlpatterns = [
     path('notifications/', include('apps.notifications.urls', namespace="notifications")),
     path('accounts/', include('allauth.urls')),
 ]
+
+if settings.DEVELOPMENT_MODE is True:
+    urlpatterns.append(
+        path("__debug__", include(debug_toolbar.urls))
+    )
